2fa51c906416070a8a7b1a058cae16336e643d55
Vasu Subbannavar <65950340+Vasu7777py@users.noreply.github.com>
2020-06-16 14:17:05 +0530

    seting up the window

--- test/exp_pygame.py
+++ test/exp_pygame.py
@@ -1,4 +1,28 @@
 
 import pygame
+import os
+import time
+
+WIDTH, HEIGHT = (750, 450)
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("...Simulation...")
 
 pygame.init()
+
+os.system("cls")
+
+def main():
+    run = True
+    FPS = 120
+    clock = pygame.time.Clock()
+
+    def redraw_window():
+        pass
+
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+                run = False
+
+main()
