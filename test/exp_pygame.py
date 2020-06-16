343d33ceba7e717f090608d63687a84c99ab893c
Vasu Subbannavar <65950340+Vasu7777py@users.noreply.github.com>
2020-06-16 15:21:30 +0530

    ...

--- test/exp_pygame.py
+++ test/exp_pygame.py
@@ -3,6 +3,15 @@ import pygame
 import os
 import time
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+class Animal(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, colour = BLACK, size = 5, velocity = [0, 0] ):
+        super.__init__()
+        pass
+
+
 WIDTH, HEIGHT = (750, 450)
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("...Simulation...")
